e796d4d5c662605a03d213653d9fd72b769ee0f6
Gavirchik <gavricenkovoleg1@gmail.com>
2026-02-04 21:08:48 +0300

    Added a new configuration setting

--- src/main/java/com/gavirchik/topordrovoseka/config/ModConfig.java
+++ src/main/java/com/gavirchik/topordrovoseka/config/ModConfig.java
@@ -5,16 +5,23 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class ModConfig {
     public static final ForgeConfigSpec SPEC;
     public static final ForgeConfigSpec.BooleanValue ENABLE_MOD;
+    public static final ForgeConfigSpec.IntValue MIN_AXE_DURABILITY;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
-        builder.comment("Mod Settings Topor Drovoseka MS3")
+        builder.comment("Mod settings Topor Drovoseka MS3")
                 .push("general");
 
         ENABLE_MOD = builder
                 .comment("Enable the mod")
                 .define("enableMod", true);
+
+        MIN_AXE_DURABILITY = builder
+                .comment("Minimum durability of a golden axe for crafting",
+                        "0 = any",
+                        "32 = only a new")
+                .defineInRange("minAxeDurability", 32, 0, 32);
 
         builder.pop();
         SPEC = builder.build();
